21e8eccc7f7c7ed7fc2d516f8e52cf7282f7edf0
Khanyisa Maluleka <khanyisamaluleka20@gmail.com>
2024-11-17 00:01:43 +0200

    Added a meme component along with its code

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react"
 import Header from "./Header.jsx"
+import Meme from "./Meme.jsx"
 
 function App() {
 
   return (
     <>
       <Header />
+      <Meme />
     </>
   )
 }
